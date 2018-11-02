1ea1d13f530e553862219f481177113b40adc0cb
Alice Pote <alice.writes.wrongs@gmail.com>
2018-11-02 10:54:50 -0400

    Custom build of CKEditor
    This is our custom changes that we want to make to CKEditor. I basically
    did these things:
    
    - got the custom build working with the plugins we want to start
    - adding the linting and code-formatting setup we use for JS
    - wrote a custom data processor so that the CKEditor takes in a spits
      out plain JS objects
    - set up webpack dev server in this repo, to use for development and
      testing
    - rewrote the README
    - set up a basic .travis.yml (currently just checks linting and runs the
      build)
    
    This should be enough for a first pass at an integration with
    open-discussions.
    
    closes #2
    closes #4
    pr #1

--- README.md
+++ README.md
@@ -1,71 +1,41 @@
-CKEditor 5 classic editor build
-========================================
+# OL CKEditor5 Custom Build
 
-[![Join the chat at https://gitter.im/ckeditor/ckeditor5](https://badges.gitter.im/ckeditor/ckeditor5.svg)](https://gitter.im/ckeditor/ckeditor5?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
-[![npm version](https://badge.fury.io/js/%40ckeditor%2Fckeditor5-build-classic.svg)](https://www.npmjs.com/package/@ckeditor/ckeditor5-build-classic)
-[![Dependency Status](https://david-dm.org/ckeditor/ckeditor5-build-classic/status.svg)](https://david-dm.org/ckeditor/ckeditor5-build-classic)
-[![devDependency Status](https://david-dm.org/ckeditor/ckeditor5-build-classic/dev-status.svg)](https://david-dm.org/ckeditor/ckeditor5-build-classic?type=dev)
+This is our custom build for CKEditor5. It's forked from their 'classic' build
+([here](https://github.com/ckeditor/ckeditor5-build-classic)).
 
-The classic editor build for CKEditor 5. Read more about the [classic editor build](https://ckeditor.com/docs/ckeditor5/latest/builds/guides/overview.html#classic-editor) and see the [demo](https://ckeditor.com/docs/ckeditor5/latest/examples/builds/classic-editor.html).
+## Development setup
 
-![CKEditor 5 classic editor build screenshot](https://c.cksource.com/a/1/img/npm/ckeditor5-build-classic.png)
+Install the deps with `yarn install`. Then if you want to check out the editor
+you can do `npm start` and visit `localhost:8080` to see a development build of
+the editor with an example document. There's both an editable and a read-only example.
 
-## Documentation
+Remember to `npm run fmt` and `npm run lint`!
 
-See:
+## Building and publishing
 
-* [Installation](https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/installation.html) for how to install this package and what it contains.
-* [Basic API](https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/basic-api.html) for how to create an editor and interact with it.
-* [Configuration](https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/configuration.html) for how to configure the editor.
-* [Creating custom builds](https://ckeditor.com/docs/ckeditor5/latest/builds/guides/development/custom-builds.html) for how to customize the build (configure and rebuild the editor bundle).
+You can run `npm run build` to run the webpack build. If you want to test the
+integration with another project, like open-discussions, you can do the
+following:
 
-## Quick start
+In this repo:
 
-First, install the build from npm:
-
-```bash
-npm install --save @ckeditor/ckeditor5-build-classic
+```
+npm run build
+rm -rf *.tgz
+npm pack
 ```
 
-And use it in your website:
+This will create a `.tgz` just like what would be pushed to the npm registry,
+but without publishing anything. We can use this in other projects to try out
+installing the package without having to push a new version up to npm.
 
-```html
-<div id="editor">
-	<p>This is the editor content.</p>
-</div>
-<script src="./node_modules/@ckeditor/ckeditor5-build-classic/build/ckeditor.js"></script>
-<script>
-	ClassicEditor
-		.create( document.querySelector( '#editor' ) )
-		.then( editor => {
-			window.editor = editor;
-		} )
-		.catch( err => {
-			console.error( err.stack );
-		} );
-</script>
+To use it, copy the `.tgz` file to your local clone of the open-discussions (or
+other project) repo and run:
+
+```
+docker-compose run watch yarn add ./name-of-the-file.tgz
 ```
 
-Or in your JavaScript application:
-
-```js
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
-// Or using the CommonJS version:
-// const ClassicEditor = require( '@ckeditor/ckeditor5-build-classic' );
-
-ClassicEditor
-	.create( document.querySelector( '#editor' ) )
-	.then( editor => {
-		window.editor = editor;
-	} )
-	.catch( err => {
-		console.error( err.stack );
-	} );
-```
-
-**Note:** If you are planning to integrate CKEditor 5 deep into your application, it is actually more convenient and recommended to install and import the source modules directly (like it happens in `ckeditor.js`). Read more in the [Advanced setup guide](https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/advanced-setup.html).
-
-## License
-
-Licensed under the terms of [GNU General Public License Version 2 or later](http://www.gnu.org/licenses/gpl.html). For full details about the license, please check the `LICENSE.md` file.
+This will add the `.tgz` file in such a way that looks, to node, like a normal
+npm package has been installed from the registry. You can then go ahead to
+include the code in OD and try it out.
--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,87 +4,99 @@
  */
 
 // The editor creator to use.
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor"
 
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials"
+import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter"
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat"
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold"
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic"
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote"
+import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage"
+import Heading from "@ckeditor/ckeditor5-heading/src/heading"
+import Image from "@ckeditor/ckeditor5-image/src/image"
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption"
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle"
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar"
+// import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload"
+import Link from "@ckeditor/ckeditor5-link/src/link"
+import List from "@ckeditor/ckeditor5-list/src/list"
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed"
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph"
+import Table from "@ckeditor/ckeditor5-table/src/table"
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar"
+
+// block toolbar setup
+import BlockToolbar from "@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar"
+import HeadingButtonsUI from "@ckeditor/ckeditor5-heading/src/headingbuttonsui"
+import ParagraphButtonUI from "@ckeditor/ckeditor5-paragraph/src/paragraphbuttonui"
+
+import JSONplugin from "./JSONDataProcessor"
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
-	Essentials,
-	UploadAdapter,
-	Autoformat,
-	Bold,
-	Italic,
-	BlockQuote,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Link,
-	List,
-	MediaEmbed,
-	Paragraph,
-	Table,
-	TableToolbar
-];
+  Essentials,
+  UploadAdapter,
+  Autoformat,
+  Bold,
+  Italic,
+  BlockQuote,
+  EasyImage,
+  Heading,
+  Image,
+  ImageCaption,
+  ImageStyle,
+  ImageToolbar,
+  // ImageUpload,
+  Link,
+  List,
+  MediaEmbed,
+  Paragraph,
+  Table,
+  TableToolbar,
+  BlockToolbar,
+  ParagraphButtonUI,
+  HeadingButtonsUI,
+  JSONplugin
+]
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'imageUpload',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
-};
+  blockToolbar: [
+    "paragraph",
+    "heading1",
+    "heading2",
+    "heading3",
+    "|",
+    "bulletedList",
+    "numberedList",
+    "|",
+    "blockQuote"
+    // "imageUpload"
+  ],
+  toolbar: {
+    items: [
+      "bold",
+      "italic",
+      "link",
+      "bulletedList",
+      "numberedList",
+      // "imageUpload",
+      "blockQuote",
+      "insertTable",
+      "mediaEmbed",
+      "undo",
+      "redo"
+    ]
+  },
+  image: {
+    toolbar: ["imageStyle:full", "imageStyle:side", "|", "imageTextAlternative"]
+  },
+  table: {
+    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"]
+  },
+  // This value must be kept in sync with the language defined in webpack.config.js.
+  language: "en"
+}
